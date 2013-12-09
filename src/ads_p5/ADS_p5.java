b06ed6c0de5ce35d6b9da064843318ac824be6ad
daandegr <daandegroot@chello.nl>
2013-12-09 16:43:00 +0100

    first

---
+++ src/ads_p5/ADS_p5.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ads_p5;
+
+/**
+ *
+ * @author Daan
+ */
+public class ADS_p5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
